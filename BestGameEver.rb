d0449e221d80aa95ad867227b6bb6bc55e259408
Flo <flo@MacBook-de-Anne.local>
2019-11-05 14:53:59 +0100

    yolo

--- BestGameEver.rb
+++ BestGameEver.rb
@@ -1,5 +1,4 @@
 require 'colorize'
-require 'juicy'
 
 class Personnage
 	
@@ -38,21 +37,27 @@ class Personnage
 	end
 
 	def blessure(degats)
-		degats = degats - @defense_perso
+		degats = degats - @defense_perso + rand(-1..1)
 		if degats <= 0
 			degats = 1
 		end
 		@pv_perso = @pv_perso - degats
+		if @pv_perso < 0
+			@pv_perso = 0
+		end
 		sleep(0.5)
 		puts "Vlan, vous perdez #{degats} pvs. Un pet de mouche...".red
 	end
 
 	def megablessure(degats)
-		degats = degats - @defense_perso + rand(5)
+		degats = degats - @defense_perso + rand(3..5)
 		if degats <= 0
 			degats = 1
 		end
 		@pv_perso = @pv_perso - degats
+		if @pv_perso < 0
+			@pv_perso = 0
+		end
 		sleep(0.5)
 		puts "violent le bougre... #{degats} pv en moins et un t shirt ruiné".red
 	end
@@ -66,12 +71,14 @@ class Personnage
 	end
 
 	def soin
+		taille_potion = rand(5..12)
 		@pv_perso = @pv_perso + 10
-		puts "Vous avalez une potion de vie pour vous remettre de ce dur combat" + " +10 PV".green
+		puts "Vous avalez une potion de vie pour vous remettre de ce dur combat" + " + #{taille_potion} PV".green
+		sleep(2)
 	end
 
 	def choice
-		puts "Bravo aventurier ! Votre adresse au combat vous donne accès à un nouvel équipement, choisissez bien...(baton, epee, arc, cuillere) "
+		puts "Bravo aventurier ! Votre adresse au combat vous donne accès à un nouvel équipement, choisissez bien...(baton, epee, arc, cuillere)"
 		sleep(0.7)
 		nouveau_jouet = gets.chomp
 		case nouveau_jouet
@@ -93,23 +100,23 @@ class Personnage
 				@defense_perso = @defense_perso*2
 		end
 		puts "Un exellent choix, vos stats augmentées sont les suivantes : #{@pv_perso} pv, #{@attaque_perso} attaque et #{@defense_perso} defense, vous êtes prêt pour l'aventure !"
-		sleep(0.5)
+		sleep(2.5)
 	end
 
 	def loot
 		if @pv_perso <= 30
-			puts "Vous dropez une énorme potion de soin" + "+ 30 pv".green
+			puts "Vous dropez une énorme potion de soin" + " + 30 pv".green
 			sleep(1)
 			@pv_perso = @pv_perso + 30
 		else 
 			random = rand(1..3)
 			if random == 1
-				puts "La chance ! Une potion de robustesse" + "+ 1 defense".green
+				puts "La chance ! Une potion de robustesse" + " + 1 defense".green
 				@defense_perso = @defense_perso + 1
 			else
-				puts "Vous trouvez une potion du berserker" + "+ 1 en attaque".green 
+				puts "Vous trouvez une potion du berserker" + " + 1 en attaque".green 
 			end
-			sleep(1)
+			sleep(3)
 		end
 	end
 end
@@ -122,7 +129,6 @@ class Monster
 		@pv_monster = pv
 		@attaque_monster = attaque
 		@defense_monster = defense 
-		pv_courants = @pv_monster
 	end
 
 	def vlan(cible)
@@ -134,14 +140,14 @@ class Monster
 	end
 
 	def blessure(degats)
-		degats = degats - @defense_monster
+		degats = degats - @defense_monster + rand(-2..2)
 		@pv_monster = @pv_monster - degats
 		sleep(0.5)
 		puts "PIF ! le vilain monstre perd #{degats} points de vie !".green
 	end
 
 	def megablessure(degats)
-		@pv_monster = @pv_monster - degats
+		@pv_monster = @pv_monster - degats + rand(-2..2)
 		sleep(0.5)
 		puts "Dans les dents ! #{degats} points de vie perdus!".green 
 	end
@@ -170,17 +176,17 @@ class Monster
 	
 end
 pvMechant = rand(50..100)
-atqMonstre = rand(3..7)
+atqMonstre = rand(4..7)
 defMonstre = rand(2..4)
-a = 100
-b = 10
-c = 4
+pvHeros = 100
+atqHeros = 10
+defHeros = 4
 
 system("clear")
 system("cls")
 
-Monstre = Monster.new(pvMechant, atqMonstre, defMonstre)
-Heros = Personnage.new(a, b, c)
+Monstre1 = Monster.new(pvMechant, atqMonstre, defMonstre)
+Heros = Personnage.new(pvHeros, atqHeros, defHeros)
 
 def combat(heros, monstre)
 
@@ -203,15 +209,16 @@ def combat(heros, monstre)
 		puts "Le boss final, tout simplement. Fuyez pauvre fou..."
 	end
 
-	sleep(0.5)
+	sleep(1)
 	puts "La bête est formidable, #{pvMechant} points de vie, une force de frappe de #{atqMonstre} et #{defMonstre} de défense ! Il va falloir jouer serré...".green.bold
+	sleep(1)
 
 	defense_base = heros.getdef
 	current_pv_monster = monstre.getpv
 	current_pv_heros = heros.getpv
 
-	until current_pv_monster == 0 || current_pv_heros == 0 
-		puts "Quelle action choisir ? (paf, block, super)"
+	while current_pv_monster > 0 && current_pv_heros > 0 
+		puts "Quelle action choisir ?" + " (paf, block, super)".light_blue
 		action = gets.chomp
 		until action == "paf" || action == "block" || action == "super"
 			action = gets.chomp
@@ -243,9 +250,11 @@ def combat(heros, monstre)
 			currrent_pv_heros = 0
 		end
 	
-		puts "Il vous reste #{current_pv_heros} points de vie, le monstre en a #{current_pv_monster}"
+		puts "Il vous reste" + " #{current_pv_heros}".green + " points de vie" + ", le monstre en a " + "#{current_pv_monster}".green
 		sleep(0.5)
-		puts "Le combat continue !"
+		if current_pv_monster > 0
+			puts "Le combat continue !"
+		end
 		sleep(0.5)
 	end
 
@@ -260,14 +269,31 @@ def combat(heros, monstre)
 	end
 end
 
-combat(Heros, Monstre)
+sleep(1.5)
+puts "Bienvenue Aventurier ! Serez vous à la hauteur du " + "ADA DUNGEON ".magenta + " ?"
+sleep(2)
+puts "Quel est votre blaze ?"
+blaze = gets.chomp
+puts "#{blaze.upcase} ? " "Un vrai nom de guerrier.re ! J'en frissone. Prêt.e pour l'aventure ?"
+answer = gets.chomp
+
+if answer == "no" || answer == "non"
+	puts "trop tard, il fallait y penser avant..."
+end
+sleep(1)
+puts "En avant " + "#{blaze}".yellow + " , que la force d'Ada soit avec toi !"
+
+sleep(2)
+system("clear")
+
+combat(Heros, Monstre1)
 
 system("clear")
 system("cls")
 Heros.soin
 Heros.choice
 pv_monstre2 = rand(75..125)
-atq_monstre2 = rand(6..9)
+atq_monstre2 = rand(5..8)
 def_monstre2 = rand(3..6)
 
 sleep(1)
@@ -282,12 +308,17 @@ while Heros.getpv > 0
 	system("clear")
 	system("cls")
 	puts "A peine remis de vos émotions, un autre monstre jaillit des ténèbres"
-	sleep(0.5)
+	sleep(1)
 	pv_monstre = rand(75..125)
-	atq_monstre = rand(3..7)
-	def_monstre = rand(2..6)
+	atq_monstre = rand(5..8) + i 
+	def_monstre = rand(3..6)
 	Monstre = Monster.new(pv_monstre, atq_monstre, def_monstre)	
 	combat(Heros, Monstre)
-	Heros.soin
+	if Heros.getpv > 0
+		Heros.soin
+	end
 end
 
+puts "Quelle aventure épique... vous avez vaincu un total de " + "#{i}".green.bold + " affreux monstres " + "ADA".magenta + " est fière de vous !"
+sleep(4)
+exit
