4afa64b3ef16ae6824840be6be58a35feabc6c4f
KheOps <kheops@ceops.eu>
2019-12-12 11:13:09 +0100

    Traduction en français de tout le texte du code
    Signed-off-by: KheOps <kheops@ceops.eu>

--- conflit_git/conflit_git.py
+++ conflit_git/conflit_git.py
@@ -1,11 +1,14 @@
-food = input("Enter your favourite food: ")
-print("Your favourite food is: {}".format(food))
+food = input("Entrez votre nourriture favorite : ")
+
+print("Votre nourriture favorite est : {}".format(food))
 
 if food == 'pomme':
-  print ("Comme Chirac!")
+  print ("Comme Chirac !")
 
 try:
     food = float(food)
     print(food)
 except:
-    print("A string cant be cast to an int/float....")
+    print("Une chaine ne peut être convertie en entier ou flottant…")
+
+
