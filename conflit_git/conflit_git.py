6e4b7c087e1d825c1ce6e30d29026a6fbedf0009
Nydragon00 <56591727+Nydragon00@users.noreply.github.com>
2019-12-12 10:35:51 +0100

    Ajout de code Python pour savoir si la chaine peut être un nombre flottant

--- conflit_git/conflit_git.py
+++ conflit_git/conflit_git.py
@@ -3,3 +3,9 @@ print("Your favourite food is: {}".format(food))
 
 if food == 'pomme':
   print ("Comme Chirac!")
+
+try:
+    food = float(food)
+    print(food)
+except:
+    print("A string cant be cast to an int/float....")
