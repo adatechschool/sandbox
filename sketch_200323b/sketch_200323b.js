fa43822dfd08b88e2389283a406d9afe7792bd97
FlorianBouteille <34886355+FlorianBouteille@users.noreply.github.com>
2020-03-24 20:07:34 +0100

    étoile

--- sketch_200323b/sketch_200323b.js
+++ sketch_200323b/sketch_200323b.js
@@ -1,3 +1,5 @@
+// un petit tas de variables globales
+
 var W = window.innerWidth;
 var H = window.innerHeight;
 var sliderAngle;
@@ -5,18 +7,37 @@ var sliderLen;
 var sliderStroke;
 var sliderLeft;
 var sliderRight;
+var sliderTimes;
+var sliderBack;
+var sliderColor;
+var sliderBackSat;
+var sliderBackBri;
+var sliderTreeSat;
+var sliderTreeBri;
+
+// je pense que c'est pas la peine d'initialiser celles là, mais bon, j'aime pas les déclarer dans la draw alors voilà :
+
 var angle = PI/4;
 var len = 100;
 var strokeValue = 12;
 var coefLeft = 0.67;
 var coefRight = 0.67;
+var backColor = 51;
+var backSat = 50;
+var backBri = 75;
+var treeColor = 255;
+var treeSat = 25;
+var treeBri = 100;
 
 function setup() 
 {
+  // Je passe en HSB par ce que c'est la tendance du moment et que ça m'économise 1 curseur
+  colorMode(HSB, 360, 100, 100);
   createCanvas(W, H);
-  sliderAngle = createSlider(0, TWO_PI, PI/4, 0.01);
+  
+  //tout ça est très répetitif, mais ça sert juste à créer les sliders
+  sliderAngle = createSlider(0, TWO_PI, PI/4, 0.01);// createSlider(minimum, maximum, valeur de départ, pas) <---- SUPER PRATIQUE
   sliderAngle.position(10, 10);
-  sliderAngle.style('background-color', "#000000");
   sliderLen = createSlider(10, 400, 100, 1);
   sliderLen.position(10, 40);
   sliderStroke = createSlider(4, 50, 10, 1);
@@ -25,36 +46,60 @@ function setup()
   sliderLeft.position(10, 100);
   sliderRight = createSlider(0.4, 0.8, 0.67, 0.01);
   sliderRight.position(10, 130);
+  sliderTimes = createSlider(0, 11, 4, 1);
+  sliderTimes.position(10, 160);
+  sliderTree = createSlider(0, 360, 0, 1);
+  sliderTree.position(W - 230, 10);
+  sliderTreeSat = createSlider(0, 100, 0, 1);
+  sliderTreeSat.position(W - 230, 40);
+  sliderTreeBri = createSlider(0, 100, 100, 1);
+  sliderTreeBri.position(W - 230, 70);
+  sliderBack = createSlider(0, 360, 0, 1);
+  sliderBack.position(W - 230, 140);
+  sliderBackSat = createSlider(0, 100, 0, 1);
+  sliderBackSat.position(W - 230, 170);
+  sliderBackBri = createSlider(0, 100, 9, 1);
+  sliderBackBri.position(W - 230, 200);
 }
 
 
 function draw() 
 {
-  background(51);
-  stroke(255);
+  // on récupère les valeurs de chaque slider à chaque appel de la draw
   angle = sliderAngle.value();
   len = sliderLen.value();
   strokeValue = sliderStroke.value();
   coefRight = sliderRight.value();
   coefLeft = sliderLeft.value();
+  Times = sliderTimes.value();
+  backColor = sliderBack.value();
+  backSat = sliderBackSat.value();
+  backBri = sliderBackBri.value();
+  treeColor = sliderTree.value();
+  treeSat = sliderTreeSat.value();
+  treeBri = sliderTreeBri.value();
+  
+  
+  background(backColor, backSat, backBri);
+  stroke(treeColor, treeSat, treeBri);
   translate(W/2, H);
-  branch(len, strokeValue);  
+  branch(len, strokeValue, Times);  
 }
 
-function branch(len, stroke)
+function branch(len, strokeW, times)
 {
-  strokeWeight(stroke);
+  strokeWeight(strokeW);
   line(0, 0, 0, - len);
   translate(0, - len);
-  if (len > 4)
+  while (times-- > 0)
   {
     push();
     rotate(angle);
-    branch(coefLeft*len, coefLeft*stroke);
+    branch(coefLeft * len, coefLeft*strokeW, times); //récursion
     pop();
     push();
     rotate(-angle);
-    branch(coefRight*len, coefRight*stroke);
+    branch(coefRight*len, coefRight*strokeW, times); // deuxième récursion
     pop();
   }
 }
