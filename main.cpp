762e42f0a2092d9f597d7b4eedabcffdbce178ed
FlorianBouteille <34886355+FlorianBouteille@users.noreply.github.com>
2020-06-03 16:35:44 +0200

    Create main.cpp
    Ca doit etre hier quand j'ai touché mes variables d'environement

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Personnage.hpp"
+
+int main()
+{
+    //créer un personnage s'il te plait ?
+    Personnage monPersonnage;
+    monPersonnage.sePresenter();
+    return 0;
+}
