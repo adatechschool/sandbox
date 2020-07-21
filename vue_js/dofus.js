e36b327af86e32dcc2b69a78a86ceaeadb63412e
FlorianBouteille <34886355+FlorianBouteille@users.noreply.github.com>
2020-07-21 15:50:52 +0200

    lool

---
+++ vue_js/dofus.js
@@ -0,0 +1,39 @@
+async function dofus_pets()
+{
+    let url = "https://fr.dofus.dofapi.fr/pets";
+    let response = await fetch(url);
+    let json = await response.json();
+    let img = [];
+    console.log(json[4]);
+    let numero = Math.floor(Math.random()*131);
+    console.log(numero);
+
+    let blaze = new Vue
+    ({
+        el: "#blaze",
+        data:
+        {
+            nom: json[numero].name
+        }
+    });
+
+    let iamg = new Vue
+    ({
+        el: "#image",
+        data:
+        {
+        image: json[numero].imgUrl
+        }
+    });
+
+    let description = new Vue
+    ({
+        el: "#description",
+        data:
+        {
+        description: json[numero].description
+        }
+    });
+}
+
+dofus_pets();
