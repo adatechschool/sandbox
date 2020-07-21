3710d6e7795dfb0aa8d978238e27b90cd0e5a919
FlorianBouteille <34886355+FlorianBouteille@users.noreply.github.com>
2020-07-21 16:33:38 +0200

    hlol jai travaillé aujourdhui

--- vue_js/dofus.js
+++ vue_js/dofus.js
@@ -3,8 +3,7 @@ async function dofus_pets()
     let url = "https://fr.dofus.dofapi.fr/pets";
     let response = await fetch(url);
     let json = await response.json();
-    let img = [];
-    console.log(json[4]);
+    console.log(json);
     let numero = Math.floor(Math.random()*131);
     console.log(numero);
 
